fc7676b875a21ff6b64c2dc0edd810cd93cda45e
Yılmaz Edis <ylmazedis@gmail.com>
2022-11-26 23:01:27 +0300

    Extract FriendsAPI logic into composition adapter

--- iACC/ListViewController.swift
+++ iACC/ListViewController.swift
@@ -4,8 +4,14 @@
 
 import UIKit
 
+protocol ItemService {
+    func loadItems(completion: @escaping (Result<[ItemViewModel], Error>) -> Void)
+}
+
 class ListViewController: UITableViewController {
     var items = [ItemViewModel]()
+    
+    var service: ItemService?
     
     var retryCount = 0
     var maxRetryCount = 0
@@ -24,15 +30,7 @@ class ListViewController: UITableViewController {
         refreshControl = UIRefreshControl()
         refreshControl?.addTarget(self, action: #selector(refresh), for: .valueChanged)
         
-        if fromFriendsScreen {
-            shouldRetry = true
-            maxRetryCount = 2
-            
-            title = "Friends"
-            
-            navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addFriend))
-            
-        } else if fromCardsScreen {
+        if fromCardsScreen {
             shouldRetry = false
             
             title = "Cards"
@@ -68,22 +66,7 @@ class ListViewController: UITableViewController {
     @objc private func refresh() {
         refreshControl?.beginRefreshing()
         if fromFriendsScreen {
-            FriendsAPI.shared.loadFriends { [weak self] result in
-                DispatchQueue.mainAsyncIfNeeded {
-                    self?.handleAPIResult(result.map({ items in
-                        
-                        if User.shared?.isPremium == true {
-                            (UIApplication.shared.connectedScenes.first?.delegate as! SceneDelegate).cache.save(items)
-                        }
-                        
-                        return items.map { item in
-                            ItemViewModel(friend: item) {
-                                self?.select(friend: item)
-                            }
-                        }
-                    }))
-                }
-            }
+            service?.loadItems(completion: handleAPIResult)
         } else if fromCardsScreen {
             CardAPI.shared.loadCards { [weak self] result in
                 DispatchQueue.mainAsyncIfNeeded {
